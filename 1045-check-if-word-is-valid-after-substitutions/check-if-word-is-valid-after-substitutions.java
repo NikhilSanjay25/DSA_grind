d3d324404f5a67d1039468649f3d0206c0ff8a85
Nikhil Sanjay <152269472+NikhilSanjay25@users.noreply.github.com>
2025-08-20 08:42:22 +0530

    Time: 74 ms (9.81%) | Memory: 45.6 MB (6.05%) - LeetSync

---
+++ 1045-check-if-word-is-valid-after-substitutions/check-if-word-is-valid-after-substitutions.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isValid(String s) {
+        while(true){
+            boolean test = false;
+            for(int i=0;i<=s.length()-3;i++){
+                if(s.substring(i,i+3).equals("abc")){
+                    s = s.substring(0,i)+s.substring(i+3,s.length());
+                    test = true;
+                }
+            }
+            if(test){
+                continue;
+            }
+            else{
+                break;
+            }
+        }
+        return s.length()==0;
+    }
+}
