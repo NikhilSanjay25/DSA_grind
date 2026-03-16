1e93b6e6525b9a73977b883bc06c3b182ea07d7b
Nikhil Sanjay <152269472+NikhilSanjay25@users.noreply.github.com>
2026-03-16 14:13:27 +0530

    Time: 8 ms (76.72%) | Memory: 46.3 MB (51.23%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -3,25 +3,18 @@ class Solution {
         ArrayList<Integer> op = new ArrayList<>();
         op.add(nums[0]);
         for(int i=1;i<nums.length;i++){
-            int index = Collections.binarySearch(op,nums[i]);
-            if(index<0){
-                index = -index - 1;
-                if(index==op.size()){
-                    op.add(nums[i]);
-                }
-                else{
-                op.set(index, nums[i]);
-                }
+            if(op.get(op.size()-1)<nums[i]){
+                op.add(nums[i]);
             }
             else{
-                if(index==op.size()){
-                    op.add(nums[i]);
+                int pointer = Collections.binarySearch(op,nums[i]);
+                if(pointer<0){
+                    pointer = -(pointer)-1;
                 }
-                else{
-                    op.set(index, nums[i]);
+                if(nums[i]<op.get(pointer)){
+                    op.set(pointer,nums[i]);
                 }
             }
-
         }
         return op.size();
     }
